adeb05261daec4f4c4508c6549e5f2bd39904cf4
Tips-Tricks-Solutions <85390738+Tips-Tricks-Solutions@users.noreply.github.com>
2021-09-24 19:38:22 +0530

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+import webbrowser
+import time
+import os,sys
+
+driver = webdriver.Chrome()
+driver.get("http://adfoc.us/67575881160500")
+time.sleep(9) 
+button = driver.find_element_by_xpath('//*[@id="showSkip"]/a/img')
+button.click()
+time.sleep (3)
+driver.quit()
+os.system ('5.py')
+exit
+
